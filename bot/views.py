238a689c79b3e7660d9ed8574dcf52868b2b4ece
Muhammad Yusuf <mohammedyousef.dev@gmail.com>
2024-08-26 23:38:21 +0500

    template and seearch

--- bot/views.py
+++ bot/views.py
@@ -1,7 +1,16 @@
 from django.views.generic import TemplateView, ListView
 from bot.models import Product
+from django.db.models import Q
+
 
 class WebAppTemplateView(ListView):
     model = Product
     context_object_name = "products"
     template_name = 'webapp.html'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get('q')
+        if query:
+            queryset = queryset.filter(title__icontains=query)
+        return queryset
