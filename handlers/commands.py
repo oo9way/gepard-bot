f998aae7d3d11f437feffe7ca0030dd01d64a9a1
Shohruz Nuraddinov <sh.nuraddinov@uic.group>
2024-10-21 23:33:13 +0500

    fix: clean

--- handlers/commands.py
+++ handlers/commands.py
@@ -14,12 +14,10 @@ async def start(update, context, user):
     message = "Здравствуйте, добро пожаловать в наш бот. \nВыберите нужную услугу."
 
     if user.is_agent:
-        await update.message.reply_text(
+        return await update.message.reply_text(
             message, reply_markup=replies.get_agent_main()
         )
-        return -1
-    await update.message.reply_text(message, reply_markup=replies.get_main())
-    return -1
+    return await update.message.reply_text(message, reply_markup=replies.get_main())
 
 
 @get_user
