e5d292b885fcc5f71697b0ed0d481be9ea06c1c9
Muhammad Yusuf <mohammedyousef.dev@gmail.com>
2024-11-12 17:26:00 +0500

    update logo

--- core/settings.py
+++ core/settings.py
@@ -172,25 +172,26 @@ AUTH_USER_MODEL = 'bot.CustomUser'
 
 JAZZMIN_SETTINGS = {
     # title of the window (Will default to current_admin_site.site_title if absent or None)
-    "site_title": "eShop",
+    "site_title": "DST",
 
     # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-    "site_header": "eShop",
+    "site_header": "DST",
 
     # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-    "site_brand": "eShop",
+    "site_brand": "DST",
 
     # Logo to use for your site, must be present in static files, used for brand on top left
-    # "site_logo": "books/img/logo.jpg",
+    "site_logo": "assets/png/logo.jpg",
 
     # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
-    "login_logo": None,
+    "login_logo": "assets/png/logo.jpg",
 
     # Logo to use for login form in dark themes (defaults to login_logo)
     "login_logo_dark": None,
 
     # CSS classes that are applied to the logo above
     "site_logo_classes": "img-circle",
+    "custom_css": "assets/css/jazzmin.css",
 
     # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
     "site_icon": None,
